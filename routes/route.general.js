c20bcd828ceeb1f31ae90b718b73b19a80507f95
Fernando Perez <fernandoperez0330@gmail.com>
2019-07-01 15:49:41 -0400

    put the body of the path method empty string

--- routes/route.general.js
+++ routes/route.general.js
@@ -5,10 +5,11 @@ let config    = require('../config/config.js');
 
 var route = function(router){
   router.get("/", async (ctx, next) => {
-    ctx.body = {
+    ctx.body = "";
+    /*ctx.body = {
         "api": "9025f462-9b44-48ee-8d64-8be8d2da13f8",
         "secret": bcrypt.hashSync("379b4bfe-9f94-4d54-b388-0b2b05de133d", config.crypt.salt.rounds)
-    };
+    };*/
   });
 };
 
