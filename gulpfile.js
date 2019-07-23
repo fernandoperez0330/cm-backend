64f7dceedc48adbb6c706fc4d1d7670699097697
Fernando Perez <fernandoperez0330@gmail.com>
2019-07-23 11:02:24 -0400

    commit the gulpfile for automation of services

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    mode = require('gulp-mode'),
+    git = require('gulp-git'),
+    bump = require('gulp-bump');
+
+gulp.task('version_patch', function(){
+  return gulp.src(['./package.json'])
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
+});
+
+gulp.task('pull-dev', function(){
+    git.pull("origin", "dev", {args: '--rebase'}, function(err){
+      if (err) throw err;
+    });
+});
